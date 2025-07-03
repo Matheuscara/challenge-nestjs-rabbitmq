28314488d9aea762136550ab0340c819d535a0c4
matheus dias cara <matheusdiascara@192.168.0.100>
2025-07-03 00:12:36 -0300

    Atualiza .gitignore para ignorar dist/, .env e demais

--- dist/domain/value-objects/notification-content.vo copy.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class NotificationContent {
-    private readonly _value;
-    private constructor();
-    static create(content: string): NotificationContent;
-    private validate;
-    get value(): string;
-}
